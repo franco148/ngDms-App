aa7bba2af96a14343876687d12af9d2042a641f2
franco-extreme <franck_ral@hotmail.com>
2018-03-29 16:47:28 -0400

    ngDms-App-Images: File validations from a directive.

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -2,13 +2,15 @@
 // ElementReft to have a direct interaction with the html
 // HostListener Events or callbacks when something happens (drag, mouse over, etc).
 // Output a response for EventEmitter
-import { Directive, EventEmitter, ElementRef, HostListener, Output } from '@angular/core';
+import { Directive, EventEmitter, ElementRef, HostListener, Output, Input } from '@angular/core';
+import { FileItem } from '../models/file-item';
 
 @Directive({
   selector: '[appNgDropFiles]'
 })
 export class NgDropFilesDirective {
 
+  @Input() files: FileItem[] = [];
   @Output() mouseOver: EventEmitter<boolean> = new EventEmitter();
 
   constructor() { }
@@ -21,5 +23,39 @@ export class NgDropFilesDirective {
   @HostListener('dragleave', ['$event'])
   public onDragLeave(event: any) {
     this.mouseOver.emit(false);
+  }
+
+  // Validations
+
+  private _fileCanBeUploaded(file: File): boolean {
+
+    if (!this._fileHasBeenDropped(file.name) && this._isImage(file.type)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private _preventOpenFile(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  private _fileHasBeenDropped(fileName: string): boolean {
+
+    for (const file of this.files) {
+
+      if (file.fileName === fileName) {
+        console.log('The file ' + fileName + ' is already added...');
+
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private _isImage(fileType: string): boolean {
+    return (fileType === '' || fileType === undefined) ? false : fileType.startsWith('image');
   }
 }
